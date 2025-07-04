fad0b98974c00b2cfea1ede6f8c166e52277486f
AllanLimaS <allan_11_lima@hotmail.com>
2025-07-04 12:59:15 -0300

    configuracao inicial Strapi

---
+++ strapi-backend/src/api/tipo-de-ensaio/controllers/tipo-de-ensaio.ts
@@ -0,0 +1,7 @@
+/**
+ * tipo-de-ensaio controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::tipo-de-ensaio.tipo-de-ensaio');
---
+++ strapi-backend/src/api/tipo-de-ensaio/services/tipo-de-ensaio.ts
@@ -0,0 +1,7 @@
+/**
+ * tipo-de-ensaio service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::tipo-de-ensaio.tipo-de-ensaio');
