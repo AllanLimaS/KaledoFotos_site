1c02ef8c9e27d64b5130c209876f44b893e237cb
AllanLimaS <allan_11_lima@hotmail.com>
2025-07-28 18:05:38 -0300

    view transition add

--- astro-frontend/astro.config.mjs
+++ astro-frontend/astro.config.mjs
@@ -6,7 +6,13 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
-  
+    plugins: [tailwindcss()],
+  },
+  image: {
+    domains: [
+      "localhost",
+      "https://aplicativos-kaledofotos-back.ca48be.easypanel.host",
+      import.meta.env.STRAPI_URL,
+    ],
+  },
 });
