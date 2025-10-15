414c6317b7903993bc13b22c7cb71039318d4dbd
AllanLimaS <allan_11_lima@hotmail.com>
2025-10-15 18:17:42 -0300

    strapi fix 4

--- strapi-backend/config/database.ts
+++ strapi-backend/config/database.ts
@@ -1,34 +1,9 @@
-import path from "path";
-
-export default ({ env }) => {
-  const isCloud = !!env("DATABASE_URL");
-  const client = isCloud ? "postgres" : "sqlite";
-
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env("DATABASE_URL"),
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
+export default ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: env("DATABASE_URL"), // pega a variável injetada pelo Cloud
+    ssl: {
+      rejectUnauthorized: false,
     },
-    sqlite: {
-      connection: {
-        filename: path.join(
-          __dirname,
-          "..",
-          env("DATABASE_FILENAME", ".tmp/data.db")
-        ),
-      },
-      useNullAsDefault: true,
-    },
-  };
-
-  return {
-    connection: {
-      client,
-      ...connections[client],
-    },
-  };
-};
+  },
+});
