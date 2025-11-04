66a7104f22f0e310162758119c175433504e1cea
AllanLimaS <49033925+AllanLimaS@users.noreply.github.com>
2025-11-03 22:57:35 -0300

    Update contentful.ts

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -2,8 +2,14 @@ import * as contentful from "contentful";
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.DEV
-    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+
+  // Conteúdos em draft + publicados 
+  accessToken: import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+  host: "preview.contentful.com"
+  
+  // Com esta configuração de baixo, o build de produção pegará apenas os conteúdos que estão publicados. 
+ // accessToken: import.meta.env.DEV
+ //   ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+ //   : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+ // host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
