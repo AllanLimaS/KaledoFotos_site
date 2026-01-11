e757bbe038f1d0dc8b5071bd0ced6dd82bbe3aac
AllanLimaS <allan_11_lima@hotmail.com>
2026-01-11 19:30:37 -0300

    adicionado sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from "@astrojs/sitemap";
+
 const strapiUrl = process.env.STRAPI_URL || "localhost";
 
 // https://astro.build/config
@@ -10,10 +12,14 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   image: {
     domains: ["localhost", strapiUrl],
   },
+
   redirects: {
     "/ensaios": "/#Ensaios",
   },
+  site: 'https://kaledofotos.com.br/',
+  integrations: [sitemap()],
 });
